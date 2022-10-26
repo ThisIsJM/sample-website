5e2e5966f3997f0c869605dbff7948465ab16506
Newbeewastaken <67249315+Newbeewastaken@users.noreply.github.com>
2022-10-26 23:29:14 +0800

    modified:   pages/person/[id]/index.tsx

--- pages/person/[id]/index.tsx
+++ pages/person/[id]/index.tsx
@@ -3,22 +3,10 @@ import Link from 'next/link'
 import React from 'react'
 import { firebaseCollectedData } from '../../../firebaseManager'
 
-const person = ({person}:  InferGetStaticPropsType<typeof getStaticProps> ) => {
-  return (
-    <>
-    <img src = {person[0].profileImage}></img>
-    <h1>{person[0].firstName} {person[0].lastName}</h1>
-    <p>Age: {person[0].age}</p>
-    <p>About Me: {person[0].aboutMe}</p>
-    <Link href = '/'>Go Back</Link>
-    </>
-  )
-}
-
 
 export const getStaticProps : GetStaticProps = async(context:any) => {
    
-    const person = firebaseCollectedData.filter(person => person.id === context.params.id )
+    const person = firebaseCollectedData.filter(person => person.id.toString() === context.params.id.toString() )
     return {
         props:{
             person
@@ -34,8 +22,20 @@ export const getStaticPaths: GetStaticPaths = async () =>{
 
     return{
         paths,
-        fallback: 'blocking'
+        fallback: true
     }
+}
+
+const person = ({person}:  InferGetStaticPropsType<typeof getStaticProps> ) => {
+  return (
+    <div>
+    <img src = {person[0].profileImage}></img>
+    <h1>{person[0].firstName} {person[0].lastName}</h1>
+    <p>Age: {person[0].age}</p>
+    <p>About Me: {person[0].aboutMe}</p>
+    <Link href = '/'>Go Back</Link>
+    </div>
+  )
 }
 
 export default person;
