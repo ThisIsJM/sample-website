297baa20d5d20b0c5a6ef755790c32b8ac765bcb
Newbeewastaken <67249315+Newbeewastaken@users.noreply.github.com>
2022-10-27 08:30:31 +0800

    modified:   pages/person/[id]/index.tsx

--- pages/person/[id]/index.tsx
+++ pages/person/[id]/index.tsx
@@ -1,32 +1,9 @@
-import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
+import { GetServerSideProps, GetStaticPaths, GetStaticProps, InferGetServerSidePropsType, InferGetStaticPropsType } from 'next'
 import Link from 'next/link'
 import React from 'react'
 import { firebaseCollectedData } from '../../../firebaseManager'
 
-
-export const getStaticProps : GetStaticProps = async(context:any) => {
-   
-    const person = firebaseCollectedData.filter(person => person.id.toString() === context.params.id.toString() )
-    return {
-        props:{
-            person
-        }
-    }
-    
-}
-
-export const getStaticPaths: GetStaticPaths = async () =>{
-
-    const ids = firebaseCollectedData.map((person: any) => person.id)
-    const paths = ids.map((id: { toString: () => any }) => ({params: {id: id.toString()}}))
-
-    return{
-        paths,
-        fallback: true
-    }
-}
-
-const person = ({person}:  InferGetStaticPropsType<typeof getStaticProps> ) => {
+const person = ({person}:  InferGetServerSidePropsType<typeof getServerSideProps> ) => {
   return (
     <div>
     <img src = {person[0].profileImage}></img>
@@ -38,4 +15,39 @@ const person = ({person}:  InferGetStaticPropsType<typeof getStaticProps> ) => {
   )
 }
 
+export const getServerSideProps: GetServerSideProps = async(context : any) => {
+
+    const person = firebaseCollectedData.filter(person => person.id.toString() === context.params.id.toString())
+
+    return{
+        props:{person}
+    }
+}
+
+// export const getStaticProps : GetStaticProps = async(context:any) => {
+   
+//     const person = firebaseCollectedData.filter(person => person.id.toString() === context.params.id.toString() )
+//     return {
+//         props:{
+//             person
+//         }
+//     }
+    
+// }
+
+// export const getStaticPaths: GetStaticPaths = async () =>{
+
+//     const ids = firebaseCollectedData.map((person: any) => person.id)
+//     const paths = ids.map((id: { toString: () => any }) => ({params: {id: id.toString()}}))
+
+//     return{
+//         paths,
+//         fallback: true
+//     }
+// }
+
 export default person;
+
+function userRouter() {
+    throw new Error('Function not implemented.')
+}
